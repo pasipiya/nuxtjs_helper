682708b6f3456883e2d1edfd86f23620dcc556ff
Pasindu <pasindu@gl.block-stars.com>
2024-10-06 22:01:39 +0530

    CHG : nuxtjs crud

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+  ],
+  runtimeConfig: {
+    public: {
+      apiUrl: 'https://mock-api.binaryboxtuts.com',
+      apiKey: 'binarybox_api_key_VzO8M31mfzUAW58MBuDkyVX68IWufWJWW7m5BqqSi3FSXHHwKeHjuXQzOC7QdACzffplQ93npFb6Q3sMQLeImXxkz3IHQDkWy1yt'
+    }
+  },
+})
